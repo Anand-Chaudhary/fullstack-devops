0ebeeaaf34868358afe0f2a8e5a0b2566e6907ff
Anand Chaudhary <wassolibhai420@gmail.com>
2026-03-29 11:04:27 +0545

    frontend steup

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className='w-full h-screen p-5 bg-black text-white'>
+      <h1 className='text-4xl text-center leading-0 tracking-tighter font-semibold underline'>Welcome to the App</h1>
+    </main>
+  )
+}
+
+export default App
