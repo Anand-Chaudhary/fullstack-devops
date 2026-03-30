389ac710310c9cd1c5fda3b6661cff37793671fa
Anand Chaudhary <wassolibhai420@gmail.com>
2026-03-30 19:04:18 +0545

    socket setup in frontend and backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,23 @@
 import dotenv from 'dotenv';
 import http from 'http';
 import app from './src/app';
+import { Server } from 'socket.io';
+import { YSocketIO } from 'y-socket.io/dist/server';
 
 dotenv.config();
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize()
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,24 @@
 import { Editor } from "@monaco-editor/react"
+import { useRef, useMemo } from "react";
+import * as Y from 'yjs';
+import { SocketIOProvider } from "y-socket.io";
+import { MonacoBinding } from "y-monaco";
+
 
 const App = () => {
+  const editorRef = useRef(null);
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText('monaco'), [ydoc]);
+
+  const handleMount = (editor: any) => { //eslint-disable-line
+    editorRef.current = editor;
+
+    const provider = new SocketIOProvider('http://localhost:3000', 'monaco', ydoc, {
+      autoConnect: true,
+    });
+    const monacoBinding = new MonacoBinding(yText, editor.getModel(), new Set([editor]), provider.awareness);
+  }
+
   return (
     <main className='w-full h-screen bg-black gap-4 p-4 flex text-white'>
       <aside className="bg-amber-100 rounded-xl h-full w-1/4" ></aside>
@@ -10,6 +28,7 @@ const App = () => {
           theme="vs-dark"
           defaultLanguage="javascript"
           defaultValue="console.log('Hello World!!')"
+          onMount={handleMount}
         />
       </section>
     </main>
